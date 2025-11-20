d00cc2d2cca3e52e8086359f95009a63790c46b0
cevin rizki octavian <linoktonius@gmail.com>
2025-11-20 14:11:15 +0700

    menambahkan file pages01.py, pages02.py, pages03.py

---
+++ pages/pages02.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import plotly.express as px
+import numpy as np
+
+st.title("Data Visualization")
+
+x = np.linspace(0, 10, 100)
+y = np.sin(x)
+
+fig, ax = plt.subplots()
+ax.plot(x, y)
+st.pyplot(fig)
